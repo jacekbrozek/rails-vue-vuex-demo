7c1987fed026e109ceb4273b66e710d43cd53229
Jacek Brozek <jac33k@gmail.com>
2020-03-02 02:17:42 +0100

    home page component

---
+++ app/javascript/src/components/pages/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for='task in tasks' v-bind:key='task.id'>
+      {{ task.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import { FETCH_TASKS } from "../../../store/actions.type";
+
+  export default {
+    components: {
+    },
+    computed: {
+      ...mapGetters([
+        'tasks',
+      ])
+    },
+    mounted() {
+      this.$store.dispatch(FETCH_TASKS)
+    }
+  }
+</script>
