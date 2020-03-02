332e361aec3d13fee6408a95f3150b0653cb307d
Jacek Brozek <jac33k@gmail.com>
2020-03-02 01:54:41 +0100

    task store utilities

---
+++ app/javascript/src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from "vue"
+import Vuex from "vuex"
+import { FETCH_TASKS } from './actions.type'
+import { SET_TASKS } from './mutations.type'
+import { Task } from '../models/post'
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    tasks: []
+  },
+  mutations: {
+    [SET_TASKS](state, tasks) {
+      state.tasks = tasks
+    },
+  },
+  getters: {
+    tasks: state => state.tasks,
+  },
+  actions: {
+    async [FETCH_TASKS]({ commit }) {
+      Task.all()
+        .then(response => response.data)
+        .then(tasks => commit(SET_TASKS, tasks))
+    }
+  }
+})
